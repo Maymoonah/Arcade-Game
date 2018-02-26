2d96afff7cd6e31385ec00e829b3a18dae435e24
Maymoonah Mohammed <maymoonah.ca@gmail.com>
2018-02-25 23:46:59 -0800

    call update method for all gems in updateEntities function

--- js/engine.js
+++ js/engine.js
@@ -94,6 +94,10 @@ var Engine = (function(global) {
             enemy.update(dt);
         });
         player.update();
+        blueGem.update();
+        greenGem.update();
+        orangeGem.update();
+        star.update();
     }
 
     /* This function initially draws the "game level", it will then call
